3d8384c293cf7fa70a085fb47d9cbdecf57bf19c
Zach Stasz <zachstasz@gmail.com>
2022-07-22 13:38:03 -0400

    D3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -14,3 +14,17 @@ suspensionCoil <- read.csv('Suspension_Coil.csv', check.names=F, stringsAsFactor
 total_summary <- suspensionCoil %>% summarise(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
 
 lot_summary <-  suspensionCoil %>% group_by(Manufacturing_Lot) %>% summarise(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+
+
+#D3
+
+t.test(suspensionCoil$PSI,mu=1500)
+
+lot1 <- subset(suspensionCoil, Manufacturing_Lot == 'Lot1')
+lot2 <- subset(suspensionCoil, Manufacturing_Lot == 'Lot2')
+lot3 <- subset(suspensionCoil, Manufacturing_Lot == 'Lot3')
+
+t.test(lot1$PSI,mu=1500)
+t.test(lot2$PSI,mu=1500)
+t.test(lot3$PSI,mu=1500)
